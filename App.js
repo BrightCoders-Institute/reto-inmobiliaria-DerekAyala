0ce2d1d092f69fed125bba83d6387bb72f9e7b3b
DerekAyala <derek.ayala12345@hotmail.com>
2023-06-12 20:16:49 -0600

    Added background color to the app
    - Modified the app's styling to include a background color.

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, NativeModules, Image } from 'react-native';
+const { StatusBarManager } = NativeModules;
 
 export default function App() {
+  const propiedades = {
+    nombre: 'The Willows',
+    direccioon: '3517 W. Gray St. Utica',
+    habitaciones: '3',
+    baños: '2',
+    superficie: '230 ft²',
+    renta: '$440/m',
+    calificacion: '4.7'
+  }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e5e5e5',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'flex-start',
+    paddingTop: Platform.OS === 'android' ? StatusBarManager.HEIGHT : 0,
+  }
 });
